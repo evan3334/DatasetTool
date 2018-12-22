abbdb0694832604f50d0e85e3b94144232256c09
Evan <evan.straw99@gmail.com>
2018-12-22 12:32:22 -0500

    Add ability to choose an existing dataset

--- app/src/main/java/pw/evan/datasettool/DatasetChooseActivity.java
+++ app/src/main/java/pw/evan/datasettool/DatasetChooseActivity.java
@@ -1,10 +1,17 @@
 package pw.evan.datasettool;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
-import android.support.v7.widget.Toolbar;
+import android.view.LayoutInflater;
 import android.view.Menu;
 import android.view.MenuInflater;
+import android.view.MenuItem;
+import android.view.View;
+import android.widget.LinearLayout;
+import android.widget.TextView;
+
+import java.io.File;
 
 public class DatasetChooseActivity extends AppCompatActivity {
 
@@ -13,12 +20,67 @@ public class DatasetChooseActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_dataset_choose);
         getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+        refresh();
     }
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
         MenuInflater inflater = getMenuInflater();
-        inflater.inflate(R.menu.menu_refreshable, menu);
+        inflater.inflate(R.menu.menu_refresh, menu);
         return super.onCreateOptionsMenu(menu);
     }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        switch (item.getItemId()) {
+            case R.id.refresh:
+                refresh();
+                return true;
+            default:
+                return super.onOptionsItemSelected(item);
+        }
+
+    }
+
+    private LinearLayout getListContainer() {
+        return findViewById(R.id.dataset_list_container);
+    }
+
+    private void clearAllDatasetsFromList() {
+        getListContainer().removeAllViews();
+    }
+
+    private void addDatasetToList(final String name) {
+        LayoutInflater inflater = getLayoutInflater();
+        View listEntry = inflater.inflate(R.layout.dataset_list_element, null);
+        TextView datasetTitle = listEntry.findViewById(R.id.dataset_title);
+        datasetTitle.setText(name);
+        listEntry.setOnClickListener(view -> onClickItem(name));
+        getListContainer().addView(listEntry);
+    }
+
+    private void refresh() {
+        clearAllDatasetsFromList();
+        File dir = getExternalFilesDir(null);
+        if (dir != null) {
+            for (File current : dir.listFiles()) {
+                if (current.isDirectory()) {
+                    String name = current.getName();
+                    File csv = new File(current, name + ".csv");
+                    if (csv.exists()) {
+                        //valid dataset (ig)
+                        addDatasetToList(name);
+                    }
+                }
+            }
+        }
+    }
+
+    private void onClickItem(String name) {
+        Intent i = new Intent(getApplicationContext(), DatasetEditActivity.class);
+        i.putExtra(DatasetEditActivity.EXTRA_DATASET_FILENAME, name);
+        startActivity(i);
+        finish();
+    }
+
 }
--- app/src/main/java/pw/evan/datasettool/DatasetEditActivity.java
+++ app/src/main/java/pw/evan/datasettool/DatasetEditActivity.java
@@ -1,11 +1,17 @@
 package pw.evan.datasettool;
 
+import android.support.v7.app.ActionBar;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
 
 public class DatasetEditActivity extends AppCompatActivity {
 
     public static final String EXTRA_DATASET_FILENAME = "datasetFilename";
+
+    private static final int REQUEST_IMAGE_CAPTURE = 1;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -14,6 +20,28 @@ public class DatasetEditActivity extends AppCompatActivity {
         String datasetFilename = getIntent().getStringExtra(EXTRA_DATASET_FILENAME);
         if(datasetFilename == null){
             finish();
+        }
+        ActionBar actionBar = getSupportActionBar();
+        if(actionBar!=null){
+            actionBar.setTitle(datasetFilename);
+        }
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        MenuInflater inflater = getMenuInflater();
+        inflater.inflate(R.menu.menu_add, menu);
+        return super.onCreateOptionsMenu(menu);
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        switch(item.getItemId()){
+            case R.id.addEntry:
+
+                return true;
+            default:
+                return super.onOptionsItemSelected(item);
         }
     }
 }
