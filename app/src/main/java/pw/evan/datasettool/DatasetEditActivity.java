c7cebdbdc4ad64260420fe78fd24242bd0e11b64
Evan <evan.straw99@gmail.com>
2018-12-23 12:48:36 -0500

    Add image capture functionality

--- app/src/main/java/pw/evan/datasettool/DatasetEditActivity.java
+++ app/src/main/java/pw/evan/datasettool/DatasetEditActivity.java
@@ -1,11 +1,22 @@
 package pw.evan.datasettool;
 
+import android.content.Intent;
+import android.net.Uri;
+import android.os.Environment;
+import android.provider.MediaStore;
+import android.support.v4.content.FileProvider;
 import android.support.v7.app.ActionBar;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuInflater;
 import android.view.MenuItem;
+
+import java.io.File;
+import java.io.IOException;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+import java.util.Locale;
 
 public class DatasetEditActivity extends AppCompatActivity {
 
@@ -18,11 +29,11 @@ public class DatasetEditActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_dataset_edit);
         String datasetFilename = getIntent().getStringExtra(EXTRA_DATASET_FILENAME);
-        if(datasetFilename == null){
+        if (datasetFilename == null) {
             finish();
         }
         ActionBar actionBar = getSupportActionBar();
-        if(actionBar!=null){
+        if (actionBar != null) {
             actionBar.setTitle(datasetFilename);
         }
     }
@@ -36,12 +47,45 @@ public class DatasetEditActivity extends AppCompatActivity {
 
     @Override
     public boolean onOptionsItemSelected(MenuItem item) {
-        switch(item.getItemId()){
+        switch (item.getItemId()) {
             case R.id.addEntry:
-
+                captureImage();
                 return true;
             default:
                 return super.onOptionsItemSelected(item);
+        }
+    }
+
+    private Uri getTempImageUri() {
+        // Create an image file name
+        String timeStamp = new SimpleDateFormat("yyyyMMdd_HHmmss", Locale.US).format(new Date());
+        String imageFileName = "JPEG_" + timeStamp + "_";
+        File storageDir = new File(getCacheDir(), "temp_pics");
+        if(!storageDir.exists()){
+            storageDir.mkdirs();
+        }
+        File image;
+        try {
+            image = File.createTempFile(
+                    imageFileName,  /* prefix */
+                    ".jpg",         /* suffix */
+                    storageDir      /* directory */
+            );
+            return FileProvider.getUriForFile(this, "pw.evan.datasettool.fileprovider", image);
+        } catch (IOException e) {
+            e.printStackTrace();
+            return null;
+        }
+    }
+
+    private void captureImage() {
+        Intent takePictureIntent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
+        if (takePictureIntent.resolveActivity(getPackageManager()) != null) {
+            Uri imageUri = getTempImageUri();
+            if(imageUri!=null) {
+                takePictureIntent.putExtra(MediaStore.EXTRA_OUTPUT, imageUri);
+                startActivityForResult(takePictureIntent, REQUEST_IMAGE_CAPTURE);
+            }
         }
     }
 }
