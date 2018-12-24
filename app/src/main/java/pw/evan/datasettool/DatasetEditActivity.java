ee016e9f57ffa8cf6bb03e40012cb36832e34420
Evan <evan.straw99@gmail.com>
2018-12-24 11:29:44 -0500

    Add better error handling for creation of image files

--- app/src/main/java/pw/evan/datasettool/DatasetEditActivity.java
+++ app/src/main/java/pw/evan/datasettool/DatasetEditActivity.java
@@ -61,19 +61,26 @@ public class DatasetEditActivity extends AppCompatActivity {
         String timeStamp = new SimpleDateFormat("yyyyMMdd_HHmmss", Locale.US).format(new Date());
         String imageFileName = "JPEG_" + timeStamp + "_";
         File storageDir = new File(getCacheDir(), "temp_pics");
+        boolean good = false;
         if(!storageDir.exists()){
-            storageDir.mkdirs();
+            good = storageDir.mkdirs();
+        } else {
+            good = true;
         }
-        File image;
-        try {
-            image = File.createTempFile(
-                    imageFileName,  /* prefix */
-                    ".jpg",         /* suffix */
-                    storageDir      /* directory */
-            );
-            return FileProvider.getUriForFile(this, "pw.evan.datasettool.fileprovider", image);
-        } catch (IOException e) {
-            e.printStackTrace();
+        if(good) {
+            File image;
+            try {
+                image = File.createTempFile(
+                        imageFileName,  /* prefix */
+                        ".jpg",   /* suffix */
+                        storageDir      /* directory */
+                );
+                return FileProvider.getUriForFile(this, "pw.evan.datasettool.fileprovider", image);
+            } catch (IOException e) {
+                e.printStackTrace();
+                return null;
+            }
+        } else {
             return null;
         }
     }
